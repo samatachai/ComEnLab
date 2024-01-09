a87e0e3f0721211952d913116f544c9ca2314ba5
Samatachai Jantarat <32636520+samatachai@users.noreply.github.com>
2024-01-09 15:09:10 +0700

    Update lab6_1.py

--- Lab06/lab6_1.py
+++ Lab06/lab6_1.py
@@ -1,1 +1,16 @@
+#
+# โปรแกรม Lab 6_1 - หลอด LED กระพริบ
+#
 
+import RPi.GPIO as GPIO
+import time
+
+LED = 12
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(LED, GPIO.OUT)
+
+while True :
+  GPIO.output (LED, True)
+  time.sleep(0.1)
+  GPIO.output(LED, False)
+  time.sleep(1) 
