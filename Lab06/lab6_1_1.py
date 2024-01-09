a7b2d19bffa76b1a3a1ece9370f8beaa2848132d
Samatachai Jantarat <32636520+samatachai@users.noreply.github.com>
2024-01-09 15:16:15 +0700

    Update lab6_1_1.py

--- Lab06/lab6_1_1.py
+++ Lab06/lab6_1_1.py
@@ -1,1 +1,15 @@
+import RPi.GPIO as GPIO
+import time
+LED = 18
+GPIO.setmode(GPIO.Board)
+GPIO.setup(LED, GPIO.OUT)
 
+try:
+	while True :
+		GPIO.output (LED, True)
+		time.sleep(0.1)
+		GPIO.output(LED, False)
+		time.sleep(1)
+except KeyboardInterrupt:
+	GPIO.cleanup()
+	print("Bye…")
