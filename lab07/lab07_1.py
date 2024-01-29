c258ad9cf90c95980f4f51d7b52dc8d4ea6f450e
Samatachai Jantarat <32636520+samatachai@users.noreply.github.com>
2024-01-29 10:06:44 +0700

    Update lab07_1.py

--- lab07/lab07_1.py
+++ lab07/lab07_1.py
@@ -1,1 +1,28 @@
+import RPi.GPIO as GPIO
+import time
+import RPi.GPIO as GPIO
+import time
+
+SW1  = 27  
+SW2  = 17
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(SW1, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(SW2, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+try:
+    while True:
+	
+        if GPIO.wait_for_edge(SW1, GPIO.FALLING):
+            print("SW1 Pressed")
+
+        elif GPIO.wait_for_edge(SW2, GPIO.FALLING): 
+            print("SW2 Pressed") 
+
+        time.sleep(0.3)
+
+except KeyboardInterrupt:
+    GPIO.cleanup()
+    print("\nBye..")
+
 
