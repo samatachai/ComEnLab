ab3313ae3f5f211c759c65b015fb0077a30543fe
Samatachai Jantarat <32636520+samatachai@users.noreply.github.com>
2024-01-29 10:09:08 +0700

    Update lab7_3.py

--- lab07/lab7_3.py
+++ lab07/lab7_3.py
@@ -1,1 +1,25 @@
+import drivers
+from time import sleep
+
+display = drivers.Lcd()
+display.lcd_clear()
+
+try:
+ while True:
+   print("Writing to LCD !!!")
+   display.lcd_display_string("Lab7-LCD", 1)
+   display.lcd_display_string("Hello World", 2)
+   sleep(2)
+   
+   display.lcd_display_string("I am Engineer", 1)
+   display.lcd_display_string("0123456789abcdef", 2)
+   sleep(2)
+	
+   display.lcd_clear()
+   sleep(2)
+
+except KeyboardInterrupt:
+
+   display.lcd_clear()
+   print("\nBye...")
 
